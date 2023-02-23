8f54c20fa3fd50998791a735697783957a1c5c29
Josua <josuasitanggang51@gmail.com>
2023-02-23 10:06:06 +0700

    create feature search books

--- bookshelf/src/components/Books.jsx
+++ bookshelf/src/components/Books.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import SearchBooks from "./SearchBooks";
 
 function Books() {
   const [books, setBooks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputValue = (e) => {
+    setInputValue(e.target.value);
+  };
 
   const bookList = async () => {
     const data = await fetch("http://localhost:3000/books");
     const response = await data.json();
     setBooks(response);
-  };
+  }; 
 
   const deleteBooks = async (bookId) => {
     try {
@@ -34,11 +40,7 @@ function Books() {
       <div className="bg-red-200 mx-auto text-center mt-20 p-10 w-[90%] mb-10">
         <h2 className="text-3xl font-bold">Book List</h2>
         <div className="flex justify-center mt-3 mb-2  ">
-          <input
-            className="w-full max-w-[180px] mr-5 rounded-lg pl-2"
-            type="text"
-            placeholder="search books"
-          />
+          <SearchBooks onChange={handleInputValue} />
           <Link
             to="/books/add "
             className="bg-gray-200 w-[120px] h-[50px] flex flex-col justify-center text-sm"
@@ -47,33 +49,44 @@ function Books() {
           </Link>
         </div>
         <div className="grid sm:grid-cols-2 gap-5 lg:grid-cols-3">
-          {books.map((book) => {
-            return (
-              <div key={book.id} className="mt-5 flex flex-col ">
-                <div className="bg-cyan-200 h-[230px] p-2">
-                  <p className="font-bold p-5 lg:text-2xl">{book.name}</p>
-                  <p className="lg:text-base">Summary : {book.summary}</p>
-                  <div className="flex justify-evenly pt-5 pb-5">
-                    <p>Year : {book.year}</p>
-                    <p>Author : {book.author}</p>
-                  </div>
-                  <div className="flex justify-around mt-2">
-                    <Link to={`/books/${book.id}`}>
-                      <button className="bg-gray-300 w-[100px] h-[30px]">
-                        Edit
+          {books
+            .filter((book) => {
+              if (inputValue === "") {
+                return book;
+              } else if (
+                book.name.toLowerCase().includes(inputValue.toLowerCase())
+              ) {
+                return book;
+              }
+              return null;
+            })
+            .map((book) => {
+              return (
+                <div key={book.id} className="mt-5 flex flex-col ">
+                  <div className="bg-cyan-200 h-[230px] p-2">
+                    <p className="font-bold p-5 lg:text-2xl">{book.name}</p>
+                    <p className="lg:text-base">Summary : {book.summary}</p>
+                    <div className="flex justify-evenly pt-5 pb-5">
+                      <p>Year : {book.year}</p>
+                      <p>Author : {book.author}</p>
+                    </div>
+                    <div className="flex justify-around mt-2">
+                      <Link to={`/books/${book.id}`}>
+                        <button className="bg-gray-300 w-[100px] h-[30px]">
+                          Edit
+                        </button>
+                      </Link>
+                      <button
+                        onClick={() => deleteBooks(book.id)}
+                        className="bg-gray-300 w-[100px] h-[30px]"
+                      >
+                        Delete
                       </button>
-                    </Link>
-                    <button
-                      onClick={() => deleteBooks(book.id)}
-                      className="bg-gray-300 w-[100px] h-[30px]"
-                    >
-                      Delete
-                    </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
       </div>
     </div>
