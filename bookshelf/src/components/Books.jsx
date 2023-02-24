119717727d2c3be7ea1865c31d71e9fd294a489e
Josua <josuasitanggang51@gmail.com>
2023-02-24 10:15:31 +0700

    custom style hover & alert deleteBooks

--- bookshelf/src/components/Books.jsx
+++ bookshelf/src/components/Books.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import SearchBooks from "./SearchBooks";
+import Swal from "sweetalert2";
 
 function Books() {
   const [books, setBooks] = useState([]);
@@ -14,7 +15,7 @@ function Books() {
     const data = await fetch("http://localhost:3000/books");
     const response = await data.json();
     setBooks(response);
-  }; 
+  };
 
   const deleteBooks = async (bookId) => {
     try {
@@ -31,6 +32,33 @@ function Books() {
     }
   };
 
+  const alertButtonDanger = (bookId) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Are you sure you want to delete this book?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: '<span class="text-white">Yes, delete it!</span>',
+      cancelButtonText: '<span class="text-white">No, cancel!</span>',
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      reverseButtons: true,
+      buttonsStyling: false,
+      customClass: {
+        confirmButton:
+          "px-4 ml-4 py-2 bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50",
+        cancelButton:
+          "px-4 py-2 bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50",
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteBooks(bookId);
+        Swal.fire("Deleted!", "Your book has been deleted.", "success");
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire("Cancelled", "Your books is safe :)", "error");
+      }
+    });
+  };
   useEffect(() => {
     bookList();
   }, []);
@@ -43,7 +71,7 @@ function Books() {
           <SearchBooks onChange={handleInputValue} />
           <Link
             to="/books/add "
-            className="bg-gray-200 w-[120px] h-[50px] flex flex-col justify-center text-sm"
+            className="bg-gray-200 w-[120px] h-[50px] flex flex-col justify-center text-sm hover:bg-gray-300"
           >
             Add Books
           </Link>
@@ -72,13 +100,13 @@ function Books() {
                     </div>
                     <div className="flex justify-around mt-2">
                       <Link to={`/books/${book.id}`}>
-                        <button className="bg-gray-300 w-[100px] h-[30px]">
+                        <button className="bg-gray-300 w-[100px] h-[30px] hover:bg-slate-200">
                           Edit
                         </button>
                       </Link>
                       <button
-                        onClick={() => deleteBooks(book.id)}
-                        className="bg-gray-300 w-[100px] h-[30px]"
+                        onClick={() => alertButtonDanger(book.id)}
+                        className="bg-gray-300 w-[100px] h-[30px] hover:bg-gray-200"
                       >
                         Delete
                       </button>
